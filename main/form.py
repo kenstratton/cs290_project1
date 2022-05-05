4db9e0250ac0efbc7886c6d0e29fb6a54b1f4fc1
kenstratton <ken.stratton.2643@mail.linbenton.edu>
2022-05-05 14:40:57 +0900

    added profile page

--- main/form.py
+++ main/form.py
@@ -1,9 +1,14 @@
+from wsgiref import validate
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-# Validation classes
+
+
+# Validation classes---------------------------------------
+
+# Length
 class ValidateLength(Length):
     def __init__(self, min, max):
         super().__init__(
@@ -11,58 +16,98 @@ class ValidateLength(Length):
             max=max,
             message=f"*Must be {min} - {max} characters long.")
 
+
+# Required
 class ValidateRequired(DataRequired):
     def __init__(self, type):
         super().__init__(
             message=f"*{type} is required.")
 
+
+# Email
 class ValidateEmail(Email):
     def __init__(self):
         super().__init__(
-            message="*Invalid address (e.g 123@example.com).")
+            message="*Invalid address (e.g email@address.com).")
 
+
+# Equality
 class ValidateEqual(EqualTo):
-    def __init__(self, target):
+    def __init__(self, type):
         super().__init__(
-            fieldname=target,
-            message=f"*Must match the {target} field.")
+            fieldname=type,
+            message=f"*Must match the {type} field.")
 
 
 
-# Form classes
+
+# Form classes --------------------------------------------
+
+# Base
 class Form(FlaskForm):
     submit = SubmitField('Submit')
 
-    def is_valid(self, target):
+    def is_valid(self, type):
         result = 0
-        if  (self[target].errors):
+        if  (self[type].errors):
                 result = 1
         return result
 
 
+# User form
 class UserForm(Form):
-    name = StringField("Nickname", [
+    name = StringField('Nickname', [
         ValidateRequired('Nickname'),
         ValidateLength(3, 100)])
 
-    email = EmailField("Email", [
+    email = EmailField('Email', [
         ValidateRequired('Email'),
         ValidateLength(3, 120),
         ValidateEmail()])
 
-    password = PasswordField("Password", [
+    password = PasswordField('Password', [
         ValidateRequired('Password'),
         ValidateLength(8, 100),
         ValidateEqual('confirm')])
 
-    confirm  = PasswordField("Confirm Password")
+    confirm  = PasswordField('Confirm Password')
 
 
+# Login form
 class UserLoginForm(Form):
-    name = StringField("Nickname", [
-        ValidateRequired('Nickname'),
+    name_or_email = StringField('Nickname / Email', [
+        ValidateRequired('Nickname / Email'),
         ValidateLength(3, 100)])
 
     password = PasswordField('Password', [
         ValidateRequired('Password'),
         ValidateLength(3, 100)])
+
+
+# Password authentification form
+class PasswordAuthForm(Form):
+    password = PasswordField('Password', [
+        ValidateRequired('Password'),
+        ValidateLength(8, 100)])
+
+
+# Update form (Name & Email)
+class UserUpdateForm(Form):
+    name = StringField('Nickname', [
+        ValidateRequired('Nickname'),
+        ValidateLength(3, 100)])
+
+    email = EmailField('Email', [
+        ValidateRequired('Email'),
+        ValidateLength(3, 120),
+        ValidateEmail()])
+
+
+# Password update form
+class PasswordUpdateForm(Form):
+    password = PasswordField('Password', [
+        ValidateRequired('Password'),
+        ValidateLength(8, 100),
+        ValidateEqual('confirm')])
+
+    confirm  = PasswordField('Confirm Password')
--- main/views.py
+++ main/views.py
@@ -1,128 +1,155 @@
-from flask import render_template, url_for, redirect, flash, session, request, Response
+from crypt import methods
+import functools
+from flask import g, flash, redirect, render_template, request, url_for, session
+from sqlalchemy import or_ 
 from main import app, db
 from main.models import User
-from main.form import UserForm, UserLoginForm
+from main.form import UserForm, UserLoginForm, PasswordAuthForm, UserUpdateForm, PasswordUpdateForm
 
+
+
+
+# In-module methods ---------------------------------------
 
 # Check the login status of a user
 def user_is_in():
-    if 'user_name' in session:
+    if 'user_id' in session:
         return True
+    return False
+
+def is_authentificated():
+    if 'authentificated' in session:
+        return True
+    return False
+
+
+
+
+# Auxiliary methods for routing ---------------------------
+
+# Get data of a user to be logged in
+@app.before_request
+def load_logged_in_user():
+    if user_is_in():
+        g.user = User.query.filter_by(id=session['user_id']).first()
     else:
-        return False
+        g.user = None
 
 
-# Top
-@app.route('/')
-def top():
-    if not user_is_in():
-        return redirect(url_for('entry'))
+# Redirect to the entry page without logged in status
+def login_required(view):
+    @functools.wraps(view)
+    def censored_view(*args, **kwargs):
+        if not user_is_in():
+            return redirect(url_for('entry'))
+        return view(*args, **kwargs)
 
-    return render_template(
-        'app/app.html', type='top', title='LOG BASE', session=session)
-
-
-# Add channel
-@app.route('/add_channel')
-def add_channel():
-    if not user_is_in():
-        return redirect(url_for('entry'))
-
-    return render_template(
-        'app/app.html', type='add_channel', title='Add Channel', session=session)
+    return censored_view
 
 
-# Search channel
-@app.route('/search')
-def search():
-    if not user_is_in():
-        return redirect(url_for('entry'))
+# Redirect to the top page with logged in status
+def is_logged_in(view):
+    @functools.wraps(view)
+    def censored_view(*args, **kwargs):
+        if user_is_in():
+            return redirect(url_for('top'))
+        return view(*args, **kwargs)
 
-    return render_template(
-        'app/app.html', type='search', title='Search', session=session)
-
-
-# Settings
-@app.route('/config')
-def config():
-    if not user_is_in():
-        return redirect(url_for('entry'))
-
-    return render_template(
-        'app/app.html', type='config', title='Settings', session=session)
+    return censored_view
 
 
-# Profile
-@app.route('/profile')
-def profile():
-    if not user_is_in():
-        return redirect(url_for('entry'))
+def authentification_required(dest):
+    def authentification_required(view):
+        @functools.wraps(view)
+        def censored_view(*args, **kwargs):
+            if not is_authentificated():
+                return redirect(url_for('password_auth', dest=dest))
+            return view(*args, **kwargs)
 
-    user = User.query.filter_by(name=session['user_name']).first()
+        return censored_view
+    return authentification_required
 
-    form = UserForm()
-    form.name.data = user.name
-    form.email.data = user.email
 
-    return render_template(
-        'app/app.html', type='profile', title='Profile', session=session, form=form)
 
+# Custom error pages --------------------------------------
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error/404.html'), 404
+
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error/500.html'), 500
+
+
+
+
+# Maintenance page ----------------------------------------
+
+# Invalid URL
+@app.route('/maintenance')
+def under_maintenace():
+    return render_template('management/maintenance.html')
+
+
+
+
+# Application ---------------------------------------------
 
 # Entry for unauthenticated users
 @app.route('/entry')
+@is_logged_in
 def entry():
-    if user_is_in():
-        return redirect(url_for('top'))
-    else:
-        return render_template(
-            'app/app.html', type='entry', title='Entry', session=session)
-
-
-# Log in
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if user_is_in():
-        return redirect(url_for('top'))
-
-    form = UserLoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
-        if user and user.verify_password(form.password.data):
-            session['user_name'] = user.name
-            return redirect(url_for('top'))
-
     return render_template(
-        'app/app.html', type='login', title='Log In', session=session, form=form)
+        'app/app.html', type='entry', title='Entry', session=session)
 
 
 # Sign up
 @app.route('/signup', methods=['GET', 'POST'])
+@is_logged_in
 def signup():
     form = UserForm()
 
-    if user_is_in():
-        return redirect(url_for('top'))
-
     if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
-        if user is None:
-            user = User(
+        existing_user = User.query.filter_by(name=form.name.data).first()
+        if existing_user is None:
+            user_to_add = User(
                 name = form.name.data,
                 email = form.email.data,
                 password = form.password.data)
-            db.session.add(user)
+            db.session.add(user_to_add)
             db.session.commit()
-            session['user_name'] = user.name
+            session['user_id'] = user_to_add.id
             flash(f'Signed up successfully!')
-            form.name.data = ''
-            form.email.data = ''
-            form.password.data = ''
             return redirect(url_for('top'))
         else:
-            flash(f'User <{user.name}> already exists!')
+            flash(f'User <{existing_user.name}> already exists!')
 
     return render_template(
         'app/app.html', type='signup', title='Sign Up', form=form, session=session)
+
+
+# Log in
+@app.route('/login', methods=['GET', 'POST'])
+@is_logged_in
+def login():
+    form = UserLoginForm()
+
+    if form.validate_on_submit():
+        user_to_login = User.query.filter(
+            or_(
+                User.name==form.name_or_email.data,
+                User.email==form.name_or_email.data
+            )).first()
+        if user_to_login and user_to_login.verify_password(form.password.data):
+            session['user_id'] = user_to_login.id
+            return redirect(url_for('top'))
+
+    return render_template(
+        'app/app.html', type='login', title='Log In', session=session, form=form)
 
 
 # Log out
@@ -132,36 +159,145 @@ def logout():
     return redirect(url_for('entry'))
 
 
+# Top
+@app.route('/')
+@login_required
+def top():
+    return render_template(
+        'app/app.html', type='top', title='LOG BASE', session=session)
 
-# Admin ------------------------------------------
 
-@app.route('/admin', methods=['GET', 'POST'])
+# Add channel
+@app.route('/add_channel')
+@login_required
+def add_channel():
+    return render_template(
+        'app/app.html', type='add_channel', title='Add Channel', session=session)
+
+
+# Search channel
+@app.route('/search')
+@login_required
+def search():
+    return render_template(
+        'app/app.html', type='search', title='Search', session=session)
+
+
+# Settings
+@app.route('/config')
+@login_required
+def config():
+    return render_template(
+        'app/app.html', type='config', title='Settings', session=session)
+
+
+# Profile
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template(
+        'app/app.html', type='profile', title='Profile', session=session, user=g.user)
+
+
+# Password Authentification
+@app.route('/profile/<string:dest>/auth', methods=['GET', 'POST'])
+@login_required
+def password_auth(dest):
+    form = PasswordAuthForm()
+
+    if request.method == 'POST':
+        user_to_verify = User.query.filter_by(id=session['user_id']).first()
+        if form.validate_on_submit \
+        and user_to_verify.verify_password(form.password.data):
+            session['authentificated'] = True
+            flash('Successfully authentificated!')
+            return redirect(url_for(dest))
+
+    return render_template(
+        'app/app.html',
+        type='profile/auth',
+        title='Profile / Authentification',
+        session=session, form=form)
+
+
+# Update Name & Email
+@app.route('/profile/basic_info', methods=['GET', 'POST'])
+@login_required
+@authentification_required('basic_info')
+def basic_info():
+    form = UserUpdateForm()
+
+    if request.method == 'GET':
+        form.name.data = g.user.name
+        form.email.data = g.user.email
+
+    elif form.validate_on_submit():
+        user_to_update = User.query.filter_by(id=g.user.id).first()
+        user_to_update.name = form.name.data
+        user_to_update.email = form.email.data
+        db.session.commit()
+        flash('Your basic info was changed.')
+
+    return render_template(
+        'app/app.html',
+        type='profile/basic_info',
+        title='Profile / Basic Info',
+        session=session, form=form)
+
+
+# Update Password
+@app.route('/profile/password', methods=['GET', 'POST'])
+@login_required
+@authentification_required('password')
+def password():
+    form = PasswordUpdateForm()
+
+    if form.validate_on_submit():
+        user_to_update = User.query.filter_by(id=g.user.id).first()
+        user_to_update.password = form.password.data
+        db.session.commit()
+        flash('Your password was changed.')
+        form.password.data = ''
+
+    return render_template(
+        'app/app.html',
+        type='profile/password',
+        title='Profile / Password',
+        session=session, form=form)
+
+
+
+
+# Admin ---------------------------------------------------
+
+# Top page
+@app.route('/admin')
 def admin():
     users = User.query.order_by(User.date_added)
     return render_template(
         'admin/admin.html', type='top', title='Admin', users=users)
 
 
+# Add User
 @app.route('/admin/add_user', methods=['GET', 'POST'])
 def add_user(user=None):
     form = UserForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
-        if user is None:
-            user = User(
+        existing_user = User.query.filter_by(name=form.name.data).first()
+        if existing_user is None:
+            user_to_add = User(
                 name=form.name.data,
                 email=form.email.data,
                 password=form.password.data)
-            db.session.add(user)
+            db.session.add(user_to_add)
             db.session.commit()
-            flash(f"<{user.name}> added successfully!")
+            flash(f"<{user_to_add.name}> added successfully!")
             form.name.data = ''
             form.email.data = ''
             form.password.data = ''
-            return redirect(url_for('admin'))
         else:
-            flash(f"User <{user.name}> alerady exists!")
+            flash(f"User <{existing_user.name}> alerady exists!")
 
     return render_template(
         'admin/admin.html', type='add_user', title='Admin / Add User', form=form)
